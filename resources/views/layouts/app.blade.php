612a16da35521d0c64b07d9ad7af1d47995428d4
Muhammad Naufal <naufalmhd24@gmail.com>
2023-04-13 17:22:47 +0700

    First commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    @include('layouts.app.header')
+
+    <div class="container">
+
+        @yield('content')
+
+        @include('layouts.app.footer')
+    </div>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\HelloController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('hello', 'App\Http\Controllers\HelloController@index');
+// Route::get('hello', [HelloController::class, 'index']);
+// Route::post('hello', [HelloController::class, 'create']);
+// Route::get('world', [HelloController::class, 'world_message']);
+
+Route::get('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'authenticate']);
+
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/create', [PostController::class, 'create']);
+Route::post('posts', [PostController::class, 'store']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::get('posts/{id}/edit', [PostController::class, 'edit']);
+Route::patch('posts/{id}', [PostController::class, 'update']);
+Route::delete('posts/{id}', [PostController::class, 'destroy']);
